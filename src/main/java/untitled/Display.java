92de269be5d3eb690d3886f8a23bc318e2c027ae
Rain Ang <rainnier.ang@gmail.com>
2017-06-24 17:43:18 +0800

    Change Display interface to abstract class

--- src/main/java/untitled/Display.java
+++ src/main/java/untitled/Display.java
@@ -1,8 +1,81 @@
 package untitled;
 
-public interface Display
+import untitled.display.Graphics;
+import untitled.display.Scene;
+
+import javax.annotation.Nonnull;
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class Display<T extends Graphics>
 {
-	void update();
+	protected final Game game;
+	protected final Logger log;
 	
-	void render(float deltaTime);
+	private State state;
+	
+	private final Map<Class<? extends State>, Scene> scenes = new HashMap<>();
+	private Scene scene;
+	
+	public Display(Game game)
+	{
+		this.game = game;
+		this.log = Logger.get("display");
+		this.scene = getFallbackScene();
+	}
+	
+	protected abstract T getGraphics();
+	
+	public abstract int getWidth();
+	
+	public abstract int getHeight();
+	
+	protected abstract void open();
+	
+	protected abstract void close();
+	
+	void update()
+	{
+		State.Handler stateHandler = game.getStateHandler();
+		if (state != stateHandler.getCurrentState())
+		{
+			state = stateHandler.getCurrentState();
+			scene.cleanUp();
+			scene = getSceneFor(state);
+			scene.setUp(state, game.getInput());
+		}
+		scene.update();
+	}
+	
+	protected void render(float deltaTime)
+	{
+		T graphics = getGraphics();
+		scene.render(state, graphics, deltaTime);
+		graphics.dispose();
+	}
+	
+	protected final void setSceneFor(@Nonnull Class<? extends State> clazz, @Nonnull Scene scene)
+	{
+		scenes.put(clazz, scene);
+	}
+	
+	@SuppressWarnings("unchecked")
+	private final Scene getSceneFor(State state)
+	{
+		Scene scene = scenes.get(state.getClass());
+		if (scene == null)
+		{
+			log.warning(
+					"No scene for state: name=%s, type=%s",
+					state.name(),
+					state.getClass()
+						 .getName()
+			);
+			scene = getFallbackScene();
+			setSceneFor(state.getClass(), scene);
+		}
+		return scene;
+	}
+	
+	protected abstract Scene getFallbackScene();
 }
